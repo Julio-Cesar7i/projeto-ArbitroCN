451cf536a3350b40f4db7a8f537b5c2b782c9603
Julio-Cesar7i <juliocesartlc0420@gmail.com>
2025-07-25 17:22:24 -0300

    resolvendo um erro no arbitros_controller

--- app/controllers/arbitros_controller.ts
+++ app/controllers/arbitros_controller.ts
@@ -30,19 +30,4 @@ export default class ArbitrosController {
     await arbitro.delete()
     return response.noContent()
   }
-  // Método para pesquisar árbitros por CPF
-  // Retorna um árbitro se encontrado, ou um erro se não encontrado
-  // Retorna um erro se o CPF não for fornecido
-  public async search ({ request, response }: HttpContext ) {
-    const { cpf } = request.qs()
-    if (!cpf) {
-      return response.status(400).json({ error: 'CPF é necessario para a pesquisa' })
-    }
-    const arbitro = await Arbitro.query().where('cpf', cpf).first()
-    if (!arbitro) {
-      return response.status(404).json({ error: 'Nenhum Arbitro encontrado!' })
-    }
-    return response.json(arbitro)
-  }
-
 }
