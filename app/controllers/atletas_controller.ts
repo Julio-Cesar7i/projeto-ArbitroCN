0f7100ce72629b893241fb34cce43cb9c2f82e64
Anderson-jf <jose.anderson1@escolar.ifrn.edu.br>
2025-07-27 12:30:35 -0300

    Criei o controller_equipes e todas as funções

--- app/controllers/atletas_controller.ts
+++ app/controllers/atletas_controller.ts
@@ -2,12 +2,12 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Atleta from '#models/atleta'
 
 export default class AtletasController {
-  // Listar atletas com filtros opcionais
+  // Função para listar os atletas de acordo com os filtros
   public async index({ request }: HttpContext) {
-    const equipeId = request.input('equipe_id')
+    const equipeId = request.input('equipe_id') 
     const jogoId = request.input('jogo_id')
     const competicaoId = request.input('competicao_id')
-
+    
     let consulta = Atleta.query()
 
     if (equipeId) {
@@ -29,14 +29,20 @@ export default class AtletasController {
     return await consulta
   }
 
-  // Cadastrar atleta
+  // Função para mostrar um atleta específico
+  public async show({ params }: HttpContext) {
+    const atleta = await Atleta.findOrFail(params.id)
+    return atleta
+  }
+
+  // Função para criar um novo atleta
   public async store({ request }: HttpContext) {
     const dados = request.only(['nome', 'data_nascimento', 'cpf', 'equipeId'])
     const atleta = await Atleta.create(dados)
     return atleta
   }
 
-  // Atualizar atleta
+  // Função para atualizar um atleta existente
   public async update({ request, params }: HttpContext) {
     const atleta = await Atleta.findOrFail(params.id)
     const dados = request.only(['nome', 'data_nascimento', 'cpf'])
@@ -45,9 +51,9 @@ export default class AtletasController {
     return atleta
   }
 
-
-  // public async show({ params }: HttpContext) {
-  //   const atleta = await Atleta.findOrFail(params.id)
-  //   return atleta
-  // }
+  // Função para deletar um atleta
+  public async destroy({ params }: HttpContext) {
+    const atleta = await Atleta.findOrFail(params.id)
+    await atleta.delete()
+  }
 }
---
+++ app/controllers/equipes_controller.ts
@@ -0,0 +1,36 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Equipe from '#models/equipe'
+
+    export default class EquipesController {
+
+        public async index({}: HttpContext) {
+            const equipes = await Equipe.all()
+            return equipes
+        }
+
+        public async show({ params }: HttpContext) {
+            const equipe = await Equipe.findOrFail(params.id)
+            return equipe
+        }
+
+        public async store({ request }: HttpContext) {
+            const dados = await Equipe.create(request.only(['nomw', 'responsavel']))
+            return dados
+        }
+
+        public async update({ request, params }: HttpContext) {
+            const equipe = await Equipe.findOrFail(params.id)
+            const dados = request.only(['nome', 'responsavel'])
+
+            equipe.merge(dados)
+            await equipe.save()
+
+            return equipe
+        }
+
+        public async destroy({ params }: HttpContext) {
+            const equipe = await Equipe.findOrFail(params.id)
+            await equipe.delete()
+        }
+    }
+
