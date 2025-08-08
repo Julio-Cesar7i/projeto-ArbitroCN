f12b639090b4f024f7a2bee0264d0e0ae1c36ceb
Julio César <132079416+Julio-Cesar7i@users.noreply.github.com>
2025-08-08 11:26:27 -0300

    alterei o validator de arbitro pq jao e boco

--- app/validators/arbitro.ts
+++ app/validators/arbitro.ts
@@ -2,10 +2,28 @@ import vine from '@vinejs/vine'
 
 export const createArbitroValidator = vine.compile(
   vine.object({
-    cpf: vine.string().minLength(11).maxLength(14),
-    nome: vine.string(),
-    email: vine.string().email(),
-    telefone: vine.string(),
+    cpf: vine
+      .string()
+      .regex(/[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/)
+      .unique(async (db, value) => {
+        const arbitro = await db.from('arbitros').where('cpf', value).first()
+        return !arbitro
+      }),
+
+    nome: vine.string().minLength(2).maxLength(100),
+
+    email: vine
+      .string()
+      .email()
+      .trim()
+      .maxLength(100)
+      .unique(async (db, value) => {
+        const arbitro = await db.from('arbitros').where('email', value).first()
+        return !arbitro
+      }),
+
+    telefone: vine.string().trim().regex(/^\d{8,9}$/),
+
     data_de_nascimento: vine.date(),
   })
-) 
+)
